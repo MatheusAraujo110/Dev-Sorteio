29ae472a188d1f5d051f58a4c2904724256ab476
MatheusAraujo110 <theusnascimento386@gmail.com>
2024-03-31 20:57:40 -0300

    Dev Sorteio

---
+++ script.js
@@ -0,0 +1,34 @@
+function generanteNumber() {
+
+    const min = Math.ceil(document.querySelector(".input-min").value)
+    const max = Math.floor(document.querySelector(".input-max").value)
+
+
+    if (min >= max) {
+        alert("O valor minímo tem que ser MENOR que o valor máximo")
+    }
+    else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+        alert(result)
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
